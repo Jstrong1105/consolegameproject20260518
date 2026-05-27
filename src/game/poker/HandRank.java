36b6abdd673a9fab5cc69a93758060d309e7dbb4
JStrong <jstrong1105@naver.com>
2026-05-27 11:25:22 +0900

    2026-05-27 11:25 카드 족보 다듬기

--- src/game/poker/HandRank.java
+++ src/game/poker/HandRank.java
@@ -11,35 +11,37 @@ public class HandRank implements Comparable<HandRank>
 	private final HandRankType type;
 	private final List<Integer> kickers;
 	
+	// 생성자
 	public HandRank(HandRankType type, List<Integer> kickers)
 	{
+		if(kickers.size() != type.getSize())
+		{
+			throw new IllegalArgumentException("키커 수 오류");
+		}
+		
 		this.type = type;
-		this.kickers = kickers; 
+		this.kickers = List.copyOf(kickers);
 	}
 	
+	// 족보 이름 반환
+	public String getName()
+	{
+		return type.getName(kickers);
+	}
+	
+	// 족보 비교
 	@Override
 	public int compareTo(HandRank o)
 	{
 		int result = this.type.getRank() - o.type.getRank();
 		
-		if(result != 0)
-		{
-			return result;
-		}
-		
-		if(this.kickers.size() != o.kickers.size())
-		{
-			throw new IllegalArgumentException("같은 족보의 키커 길이가 다름");
-		}
+		if(result != 0){return result;}
 		
 		for(int i = 0; i < this.kickers.size(); i++)
 		{
 			result = this.kickers.get(i) - o.kickers.get(i);
 			
-			if(result != 0)
-			{
-				return result;
-			}
+			if(result != 0){return result;}
 		}
 		
 		return 0;
@@ -48,41 +50,19 @@ public class HandRank implements Comparable<HandRank>
 	@Override
 	public boolean equals(Object o)
 	{
-		if(o == null)
-		{
-			return false;
-		}
-		
-		if(this == o)
-		{
-			return true;
-		}
-		
-		if(this.getClass() != o.getClass())
-		{
-			return false;
-		}
+		if(o == null){return false;}
+		if(this == o){return true;}
+		if(this.getClass() != o.getClass()){return false;}
 		
 		HandRank emp = (HandRank)o;
 		
-		if(this.type != emp.type)
-		{
-			return false;
-		}
-		
-		if(this.kickers.size() != emp.kickers.size())
-		{
-			return false;
-		}
+		if(this.type != emp.type){return false;}
 		
 		for(int i = 0; i < this.kickers.size(); i++)
 		{
 			int result = this.kickers.get(i) - emp.kickers.get(i);
 			
-			if(result != 0)
-			{
-				return false;
-			}
+			if(result != 0){return false;}
 		}
 		
 		return true;
--- src/game/poker/HandRankType.java
+++ src/game/poker/HandRankType.java
@@ -1,40 +1,86 @@
 package game.poker;
 
+import java.util.List;
+import java.util.function.Function;
+
 /**
  * 족보 리스트
  */
 public enum HandRankType
 {
-	ROYAL_FLUSH("로얄플러시",12)
-	, STRAIGHT_FLUSH("스트레이트플러시",11)
-	, FOUR_OF_A_KIND("포카드",10)
-	, FULL_HOUSE("풀하우스",9)
-	, FLUSH("플러시",8)
-	, MOUNTAIN("마운틴",7)
-	, STRAIGHT("스트레이트",6)
-	, BACK_STRAIGHT("백스트레이트",5)
-	, THREE_OF_A_KIND("트리플",4)
-	, TWO_PAIR("투페어",3)
-	, ONE_PAIR("원페어",2)
-	, HIGH_CARD("하이카드",1)
+	ROYAL_FLUSH((kickers)->
+	{
+		return "로얄플러시";
+	},12,1)
+	, STRAIGHT_FLUSH((kickers)->
+	{
+		return kickers.get(0) + " 스트레이트플러시";
+	},11,1)
+	, FOUR_OF_A_KIND((kickers)->
+	{
+		return kickers.get(0) + " 포카드";
+	},10,2)
+	, FULL_HOUSE((kickers)->
+	{
+		return kickers.get(0) + "," + kickers.get(1) + " 풀하우스";
+	},9,2)
+	, FLUSH((kickers)->
+	{
+		return kickers.get(0) + " 플러시";
+	},8,5)
+	, MOUNTAIN((kickers)->
+	{
+		return "마운틴";
+	},7,1)
+	, STRAIGHT((kickers)->
+	{
+		return kickers.get(0) + " 스트레이트";
+	},6,1)
+	, BACK_STRAIGHT((kickers)->
+	{
+		return "백스트레이트";
+	},5,1)
+	, THREE_OF_A_KIND((kickers)->
+	{
+		return kickers.get(0) + " 트리플";
+	},4,3)
+	, TWO_PAIR((kickers)->
+	{
+		return kickers.get(0) + "," + kickers.get(1) + " 투페어"; 
+	},3,3)
+	, ONE_PAIR((kickers)->
+	{
+		return kickers.get(0) + " 원페어";
+	},2,4)
+	, HIGH_CARD((kickers)->
+	{
+		return kickers.get(0) + " 하이카드";
+	},1,5)
 	;
 	
-	HandRankType(String name, int rank)
+	HandRankType(Function<List<Integer>, String> name, int rank, int size)
 	{
 		this.name = name;
 		this.rank = rank;
+		this.size = size;
 	}
 	
-	private final String name;
+	private final Function<List<Integer>, String> name;
 	private final int rank;
+	private final int size;
 	
-	public String getName()
+	public String getName(List<Integer> kickers)
 	{
-		return name;
+		return name.apply(kickers);
 	}
 	
 	public int getRank()
 	{
 		return rank;
+	}
+	
+	public int getSize()
+	{
+		return size;
 	}
 }
